fd06b27e769bcc875b9f69ac46fdb8b0d8aac3f1
antheiz <salupacode@gmail.com>
2023-09-12 12:38:07 +0700

    feat: update new design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './node_modules/preline/dist/*.js'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				poppins: ['Poppins']
+			}
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('preline/plugin'),
+	],
 }
